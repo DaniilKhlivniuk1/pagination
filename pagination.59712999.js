eb94bce50177d9269506fc65d2d05c2a6083a453
DaniilKhlivniuk1 <141495344+DaniilKhlivniuk1@users.noreply.github.com>
2025-04-19 11:07:00 +0300

    Updates

--- pagination.59712999.js
+++ pagination.59712999.js
@@ -160,11 +160,11 @@
       });
     }
   }
-})({"aaKYI":[function(require,module,exports,__globalThis) {
+})({"9JJ5D":[function(require,module,exports,__globalThis) {
 var global = arguments[3];
 var HMR_HOST = null;
 var HMR_PORT = null;
-var HMR_SERVER_PORT = 63277;
+var HMR_SERVER_PORT = 1234;
 var HMR_SECURE = false;
 var HMR_ENV_HASH = "d6ea1d42532a7575";
 var HMR_USE_SSE = false;
@@ -668,6 +668,6 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 
 },{}],"4M6V8":[function(require,module,exports,__globalThis) {
 
-},{}]},["aaKYI","4M6V8"], "4M6V8", "parcelRequire940f", {})
+},{}]},["9JJ5D","4M6V8"], "4M6V8", "parcelRequire940f", {})
 
 //# sourceMappingURL=pagination.59712999.js.map
